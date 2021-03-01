273f333c84966fe5b54d53adbf1d50baf2da9694
longvt <longvu2000.hust@gmail.com>
2021-03-01 19:47:49 +0700

    Add UI list skill page

--- lib/widgets/main_screens/learning_widget/list_skill_widget.dart
+++ lib/widgets/main_screens/learning_widget/list_skill_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'skill_item.dart';
+import 'package:bordered_text/bordered_text.dart';
 
 class ListSkillWidget extends StatelessWidget {
   final Map roadMapItem;
@@ -12,47 +14,129 @@ class ListSkillWidget extends StatelessWidget {
       body: SingleChildScrollView(
         child: SafeArea(
           child: Container(
-
-            child: Column(
+            child: Stack(
               children: [
                 Container(
-                  height: 250,
+                  height: ScreenUtil().setHeight(270),
+                  width: double.infinity,
                   decoration: BoxDecoration(
+                    color: Color(0xfffafafa),
                       image: DecorationImage(
                           image: NetworkImage(
                               "https://images.unsplash.com/photo-1579202673506-ca3ce28943ef"),
                           fit: BoxFit.cover)),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  child:
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      RaisedButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        child: Icon(Icons.arrow_back),
-                      ),
-                      RaisedButton(
-                        onPressed: () {},
-                        child: Icon(
-                          Icons.favorite,
-                          color: Colors.pink,
+                      Container(
+                        margin: EdgeInsets.only(
+                            top: ScreenUtil().setHeight(8),
+                            left: ScreenUtil().setWidth(24)),
+                        child: ClipOval(
+                          child: Material(
+                            color: Colors.white, // button color
+                            child: InkWell(
+                              splashColor: Color(0xffffbf2f), // inkwell color
+                              child: SizedBox(
+                                  width: 36,
+                                  height: 36,
+                                  child: Icon(Icons.arrow_back)),
+                              onTap: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                          ),
                         ),
-                      )
+                      ),
+                      // Text(roadMapItem['name']),
+                      Container(
+                          margin:
+                          EdgeInsets.only(top: ScreenUtil().setHeight(24)),
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Stack(
+                              children: <Widget>[
+                                // Stroked text as border.
+                                Positioned(
+                                  top: 4,
+                                  child: BorderedText(
+                                    strokeWidth: 1,
+                                    strokeColor: Color(0xffffbf2f),
+                                    child: Text(roadMapItem['name'],
+                                        style: TextStyle(
+                                          fontFamily: 'SFProDisplay',
+                                          color: Colors.white,
+                                          fontSize: ScreenUtil().setSp(26),
+                                          fontWeight: FontWeight.w900,
+                                          fontStyle: FontStyle.normal,
+                                          letterSpacing: -0.52,
+                                        )),
+                                  ),
+                                ),
+                                // Solid text as fill.
+                                Text(roadMapItem['name'],
+                                    style: TextStyle(
+                                      fontFamily: 'SFProDisplay',
+                                      color: Color(0xffffbf2f),
+                                      fontSize: ScreenUtil().setSp(26),
+                                      fontWeight: FontWeight.w900,
+                                      fontStyle: FontStyle.normal,
+                                      letterSpacing: -0.52,
+                                    )),
+                              ],
+                            ),
+                          )),
                     ],
                   ),
+                      // RaisedButton(
+                      //   onPressed: () {
+                      //     Navigator.pop(context);
+                      //   },
+                      //   child: Icon(Icons.arrow_back),
+                      // ),
                 ),
-                Text(roadMapItem['name']),
                 Container(
-                  height: 500,
+                  margin: EdgeInsets.only(top: ScreenUtil().setHeight(122)),
+                  padding: EdgeInsets.only(top:ScreenUtil().setHeight(16),left:ScreenUtil().setWidth(17),right:ScreenUtil().setWidth(17)),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(24),
+                      topRight: Radius.circular(24),
+                    ),
+                    color: Colors.white,
+                  ),
+                  height: ScreenUtil().setHeight(596),
                   child: GridView.count(
                     crossAxisCount: 2,
-                    childAspectRatio: (1 / 1),
+                    childAspectRatio: (149 / 190),
                     children:
                         List.generate(roadMapItem['listSkill'].length, (index) {
                       return SkillItem(roadMapItem, index);
                     }),
                   ),
                 ),
+                Container(
+                    margin: EdgeInsets.only(
+                        top: ScreenUtil().setHeight(644),
+                        left: ScreenUtil().setWidth(24)),
+                    width: ScreenUtil().setWidth(312),
+                    height: ScreenUtil().setHeight(50),
+                    child: RaisedButton(
+                        onPressed: () {},
+                        color: Color(0xffffbf2f),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Text("Submit Proof",
+                            style: TextStyle(
+                              fontFamily: 'SFProDisplay',
+                              color: Color(0xffffffff),
+                              fontSize: ScreenUtil().setSp(18),
+                              fontWeight: FontWeight.w700,
+                              fontStyle: FontStyle.normal,
+                            )))),
               ],
             ),
           ),
--- lib/widgets/main_screens/learning_widget/skill_item.dart
+++ lib/widgets/main_screens/learning_widget/skill_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'list_course_widget.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SkillItem extends StatelessWidget {
   final Map roadMapItem;
@@ -18,23 +19,103 @@ class SkillItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.red),
-        // borderRadius: BorderRadius.circular(100)
-      ),
-      child: InkWell(
-        onTap: () {
-          changeScreen(context);
-        },
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(roadMapItem['listSkill'][index]['name']),
-            ],
-          ),
+      margin: EdgeInsets.only(right: ScreenUtil().setWidth(7),top: ScreenUtil().setHeight(20),left:ScreenUtil().setWidth(7),),
+        width: 149,
+        height: 190,
+        decoration: new BoxDecoration(
+          color: Color(0xffffffff),
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: [BoxShadow(
+              color: Color(0x26454545),
+              offset: Offset(0,3),
+              blurRadius: 6,
+              spreadRadius: 0
+          ) ],
         ),
-      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: ScreenUtil().setHeight(9),right: ScreenUtil().setWidth(9), left: ScreenUtil().setWidth(9)),
+            width: ScreenUtil().setWidth(131),
+            height: ScreenUtil().setHeight(99),
+            decoration: new BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              image: DecorationImage(
+                image: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRw7L7A7_u4dd2X675qYCf-vIS0-OJzC64uHQ&usqp=CAU"),
+                fit: BoxFit.fill,
+                alignment: Alignment.topCenter,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: ScreenUtil().setHeight(12),bottom: ScreenUtil().setHeight(4), left: ScreenUtil().setWidth(9)),
+            child: Text(roadMapItem['listSkill'][index]['name'],
+                style: TextStyle(
+                  fontFamily: 'SFProDisplay',
+                  color: Color(0xff000000),
+                  fontSize: ScreenUtil().setSp(15),
+                  fontWeight: FontWeight.w700,
+                  fontStyle: FontStyle.normal,
+
+
+                )
+            ),
+          ),
+          Stack(
+            children:[
+            Container(
+              margin: EdgeInsets.only(left: ScreenUtil().setWidth(12)),
+              width: ScreenUtil().setWidth(131),
+              height: ScreenUtil().setHeight(57),
+              child: Text("Content - Content - Content\nContent",
+                  style: TextStyle(
+                    fontFamily: 'SFProDisplay',
+                    color: Color(0xff888888),
+                    fontSize: ScreenUtil().setSp(12),
+                    fontWeight: FontWeight.w400,
+                    fontStyle: FontStyle.normal,
+
+
+                  )
+              ),
+            ),
+              Positioned(
+                // left: ScreenUtil().setWidth(110),
+                // top: ScreenUtil().setHeight(28),
+                right: ScreenUtil().setWidth(1),
+                bottom: ScreenUtil().setHeight(8),
+
+                child: Container(
+                  width: ScreenUtil().setWidth(32),
+                  height: ScreenUtil().setHeight(32),
+                  decoration: new BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/start_learn_icon.png"),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              ),
+          ]
+          )
+
+        ],
+      )
+
+      // child: InkWell(
+      //   onTap: () {
+      //     changeScreen(context);
+      //   },
+      //   child: Center(
+      //     child: Column(
+      //       mainAxisAlignment: MainAxisAlignment.center,
+      //       children: [
+      //         Text(roadMapItem['listSkill'][index]['name']),
+      //       ],
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
