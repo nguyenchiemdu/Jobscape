c99a67b74abcf59df5a8bcdd39a9d01c94219f6b
longvt <longvu2000.hust@gmail.com>
2021-03-05 22:16:37 +0700

    Finish UI workshop screen

--- lib/widgets/main_screens/workshop_widget/workshop_widget_main.dart
+++ lib/widgets/main_screens/workshop_widget/workshop_widget_main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:learning_app/widgets/main_screens/workshop_widget/workshop_past_widget.dart';
+import 'package:learning_app/widgets/main_screens/workshop_widget/workshop_reminder_widget.dart';
 import 'workshop_widget.dart';
 class WorkshopWidget extends StatelessWidget {
   @override
@@ -13,36 +16,96 @@ class WorkshopWidget extends StatelessWidget {
       //     ],
       //   ),
       body: SafeArea(
-        child: DefaultTabController(
-          length: 3,
-          // height: 500,
-          child: Column(
-            children: [
-              Container(
-                // width: 200,
-                child: TabBar(
-                  indicatorColor: Colors.yellow,
-                  
-                  tabs: [
-                    Tab(child: Text('Past Workshops'),),
-                    Tab(child: Text('Upcoming Workshops'),),
-                    Tab(child: Text('Reminders'),),
-                    // Tab(icon: Icon(Icons.directions_bike)),
-                  ],
+        child: SingleChildScrollView(
+          child: DefaultTabController(
+            length: 3,
+            initialIndex: 1,
+            // height: 500,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/top_background.png"),
+                  fit: BoxFit.fitWidth,
+                  alignment: Alignment.topCenter,
                 ),
               ),
-              Container(
-                height: 600,
-                child: TabBarView(
-                
+              child: Column(
                 children: [
-                  Workshop('past'),
-                  Workshop('upcoming'),
-                  Icon(Icons.directions_car),
+                  Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xffffffff),
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [BoxShadow(
+                        color: Color(0x1a454545),
+                        offset: Offset(0,5),
+                        blurRadius: 6,
+                        spreadRadius: 0
+                    )],
+                  ),
+                    margin: EdgeInsets.only(top:ScreenUtil().setHeight(110)),
+                    width: ScreenUtil().setWidth(312),
+                    height: ScreenUtil().setHeight(50),
+
+                    // width: 200,
+                    child: TabBar(
+                      indicator: BoxDecoration(
+                          color: Color(0xffffbf2f),
+                          borderRadius: BorderRadius.circular(10),
+                      ),
+
+                      tabs: [
+                        Tab(child: Text('Past Workshops',
+                            style: TextStyle(
+                          fontFamily: 'SFProDisplay',
+                              color: Color(0xff000000),
+                          fontSize: ScreenUtil().setSp(13),
+                          fontWeight: FontWeight.w600,
+                          fontStyle: FontStyle.normal,
+                        ),
+                          textAlign: TextAlign.center,
+
+                        ),
+
+                        ),
+                        Tab(child: Text('Upcoming Workshops',
+                            style: TextStyle(
+                              fontFamily: 'SFProDisplay',
+                              color: Color(0xff000000),
+                              fontSize: ScreenUtil().setSp(13),
+                              fontWeight: FontWeight.w400,
+                              fontStyle: FontStyle.normal,
+                            ),
+                          textAlign: TextAlign.center,
+                        ),
+                        ),
+                        Tab(child: Text('Reminders',
+                            style: TextStyle(
+                              fontFamily: 'SFProDisplay',
+                              color: Color(0xff000000),
+                              fontSize: ScreenUtil().setSp(13),
+                              fontWeight: FontWeight.w400,
+                              fontStyle: FontStyle.normal,
+                            ),
+                          textAlign: TextAlign.center,
+                        ),),
+                        // Tab(icon: Icon(Icons.directions_bike)),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 600,
+                    child: TabBarView(
+
+                    children: [
+                      PastWorkshop('past'),
+                      Workshop('upcoming'),
+                    RemindWorkshop('past'),
+                    ],
+                  ),
+                  ),
                 ],
               ),
-              ),
-            ],
+            ),
           ),
         ),
       ),
